8770f1111ac339bb4a368b94d172e0b9baff6c4c
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2015-11-17 19:49:33 +0300

    Require PHP 5.4.0 at least

--- application.php
+++ application.php
@@ -1,5 +1,13 @@
 #!/usr/bin/env php
 <?php
+//  PHP versions below 5.4.0 are not supported
+if (version_compare(PHP_VERSION, '5.4.0') === -1){
+	echo 'This application requires at least PHP 5.4.0' . PHP_EOL;
+	echo 'You are currently running ' . PHP_VERSION . '. Please update your PHP version.' . PHP_EOL;
+	exit(50);
+}
+
+
 // symlinks are not resolved by PHP properly
 // getcwd always reports source and not target
 if (isset($_SERVER['PWD'])){
