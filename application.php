cbe297e557cc4bc33909849d85115c29f2e8071d
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2015-12-03 03:06:26 +0300

    Add license headers

--- application.php
+++ application.php
@@ -1,5 +1,25 @@
 #!/usr/bin/env php
 <?php
+/**
+ * @author Victor Dubiniuk <dubiniuk@owncloud.com>
+ *
+ * @copyright Copyright (c) 2015, ownCloud, Inc.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
 //  PHP versions below 5.4.0 are not supported
 if (version_compare(PHP_VERSION, '5.4.0') === -1){
 	echo 'This application requires at least PHP 5.4.0' . PHP_EOL;
