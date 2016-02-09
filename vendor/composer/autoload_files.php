99b7f395471728fdbe8a52c9fd90c74b6ede5956
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2016-02-09 17:49:28 +0300

    Use hash_equals for comparing the token

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,6 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'ad155f8f1cf0d418fe49e248db8c661b' => $vendorDir . '/react/promise/src/functions_include.php',
-    '5255c38a0faeba867671b61dfda6d864' => $vendorDir . '/paragonie/random_compat/lib/random.php',
+    $vendorDir . '/react/promise/src/functions_include.php',
+    $vendorDir . '/paragonie/random_compat/lib/random.php',
+    $vendorDir . '/symfony/polyfill-php56/bootstrap.php',
 );
