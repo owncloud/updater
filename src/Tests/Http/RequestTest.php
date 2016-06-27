0c760b750c74ab5b34ca917098b5a9c99b44e189
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2016-06-27 17:51:09 +0300

    Add more tests to Request class

--- src/Tests/Http/RequestTest.php
+++ src/Tests/Http/RequestTest.php
@@ -23,4 +23,63 @@ class RequestTest extends \PHPUnit_Framework_TestCase {
 		$actual = $request->postParameter($key);
 		$this->assertEquals($expected, $actual);
 	}
+
+	public function serverProvider(){
+		return [
+			[ [], 'abcd', null ],
+			[ [ 'headers'=> [ 'command' => 'jump'] ], 'dummy',  null ],
+			[ [ 'headers'=> [ 'command' => 'jump'] ], 'command', 'jump' ],
+			[ [ 'headers'=> [ 'testArray' => ['key' => 'value'] ] ], 'testArray',  ['key' => 'value'] ],
+		];
+	}
+
+	/**
+	 * @dataProvider serverProvider
+	 */
+	public function testServerVar($vars, $key, $expected){
+		$request = new Request($vars);
+		$actual = $request->server($key);
+		$this->assertEquals($expected, $actual);
+	}
+
+	public function headerProvider(){
+		return [
+			[ [], 'meow', null ],
+			[ [ 'headers'=> [ 'command' => 'jump'] ], 'dummy',  null ],
+			[ [ 'headers'=> [ 'command' => 'jump'] ], 'command', null ],
+			[ [ 'headers'=> [ 'testArray' => ['key' => 'value'] ] ], 'testArray',  null ],
+			[ [ 'headers'=> [ 'HTTP_TESTARRAY' => ['key' => 'value'] ] ], 'testArray', ['key' => 'value'] ],
+		];
+	}
+
+	/**
+	 * @dataProvider headerProvider
+	 */
+	public function testHeaderVar($vars, $key, $expected){
+		$request = new Request($vars);
+		$actual = $request->header($key);
+		$this->assertEquals($expected, $actual);
+	}
+
+
+	public function hostProvider(){
+		return [
+			[ [ 'headers'=> [ 'SERVER_NAME' => 'jump' ] ], 'jump', null ],
+			[ [ 'headers'=> [ 'HTTP_HOST'=> 'duck', 'SERVER_NAME' => 'jump'] ], 'duck' ],
+			[ [ 'headers'=> [ 'HTTP_X_FORWARDED_HOST'=>'go', 'HTTP_HOST'=> 'duck', 'SERVER_NAME' => 'jump'] ], 'go' ],
+			[ [ 'headers'=> [ 'HTTP_X_FORWARDED_HOST'=>'go,', 'HTTP_HOST'=> 'duck', 'SERVER_NAME' => 'jump'] ], 'go' ],
+			[ [ 'headers'=> [ 'HTTP_X_FORWARDED_HOST'=>'run,forrest,run', 'HTTP_HOST'=> 'duck', 'SERVER_NAME' => 'jump'] ], 'run' ],
+		];
+	}
+
+	/**
+	 * @dataProvider hostProvider
+	 * @param $vars
+	 * @param $expected
+	 */
+	public function testGetHost($vars, $expected){
+		$request = new Request($vars);
+		$actual = $request->getHost();
+		$this->assertEquals($expected, $actual);
+	}
 }
