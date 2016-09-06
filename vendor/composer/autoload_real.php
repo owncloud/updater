3c9684d3535d8d145a2149e347da21cf881aef4f
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2016-09-06 19:52:38 +0300

    Update guzzlehttp/guzzle to 5.3.1

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -41,19 +41,15 @@ class ComposerAutoloaderInit9e15cc7939314ff0b73b74491d597f46
         $loader->register(true);
 
         $includeFiles = require __DIR__ . '/autoload_files.php';
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire9e15cc7939314ff0b73b74491d597f46($fileIdentifier, $file);
+        foreach ($includeFiles as $file) {
+            composerRequire9e15cc7939314ff0b73b74491d597f46($file);
         }
 
         return $loader;
     }
 }
 
-function composerRequire9e15cc7939314ff0b73b74491d597f46($fileIdentifier, $file)
+function composerRequire9e15cc7939314ff0b73b74491d597f46($file)
 {
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-    }
+    require $file;
 }
